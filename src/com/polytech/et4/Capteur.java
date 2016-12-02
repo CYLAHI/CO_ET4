e93841b7e61626506153cbfce2e5b81ddb80fa21
CYLAHI <cyrille.laure@gmail.com>
2016-12-02 11:01:50 +0100

    Create Capteur.java

---
+++ src/com/polytech/et4/Capteur.java
@@ -0,0 +1,15 @@
+package com.polytech.et4;
+
+import com.polytech.et4.ElemRegulation;
+
+class Capteur
+{
+    ElemRegulationCapteur m_elem;
+
+    void notifie(){
+	m_elem.notifie();
+    }
+
+    void setElemRegulation(){
+    }
+}
